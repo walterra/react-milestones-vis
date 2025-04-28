7a5f372075ef267dc83ac05fd0934b4ef0d86a5e
Walter M. Rafelsberger <walter@rafelsberger.at>
2025-04-28 07:09:45 +0200

    improve vs code setup

---
+++ src/setupTests.ts
@@ -0,0 +1,5 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toBeInTheDocument();
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
