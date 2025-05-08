b3c7fbcdf702b25330c9b294940449e21495ab92
Walter M. Rafelsberger <walter@rafelsberger.at>
2025-05-09 00:12:31 +0200

    cleanup rollup.config-example.js

--- rollup.config-example.js
+++ rollup.config-example.js
@@ -1,7 +1,6 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
@@ -23,7 +22,6 @@ export default {
     },
   },
   plugins: [
-    // peerDepsExternal(),
     resolve({
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
     }),
@@ -38,7 +36,7 @@ export default {
     typescript({
       tsconfig: './tsconfig.json',
       sourceMap: true,
-      // inlineSources: isDevelopment,
+      inlineSources: isDevelopment,
     }),
     babel({
       babelHelpers: 'bundled',
@@ -46,5 +44,4 @@ export default {
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
     }),
   ],
-  // external: ['react', 'react-dom', 'd3-milestones'],
 };
